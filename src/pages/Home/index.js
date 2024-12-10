4c30dfd5e53fe6f264e05a787f5183cff6e135b1
Sab <s.svrn9@proton.me>
2024-12-10 12:09:16 +0100

    Resolution bug d'affichage du msg de confirmation du formuaire + warnings

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,8 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { last } = useData()
+
   return <>
     <header>
       <Menu />
@@ -115,14 +116,15 @@ const Page = () => {
     </main>
     <footer className="row">
       <div className="col presta">
-        <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
-          label="boom"
-        />
+        <h3>Notre dernière prestation</h3>
+        {last &&
+          (<EventCard
+            imageSrc={last?.cover}
+            title={last?.title}
+            date={new Date(last?.date)}
+            small
+            label="boom"
+          />)}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
